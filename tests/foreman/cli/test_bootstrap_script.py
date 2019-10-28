e39c5c6ab5e802d441311e916f77cd6f6a683685
Stephen Wadeley <swadeley@redhat.com>
2019-10-28 17:14:06 +0000

    Added domain and fqdn

--- tests/foreman/cli/test_bootstrap_script.py
+++ tests/foreman/cli/test_bootstrap_script.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-"""Test for bootstrap script (bootstrap.py)
+"""Test for bootstrap script (bootstrap.py).
 
 :Requirement: Bootstrap Script
 
@@ -18,9 +18,12 @@
 from robottelo.decorators import stubbed, tier1, tier4, upgrade
 from robottelo.test import CLITestCase
 from robottelo.config import settings
-from nailgun import client, entities
-
-
+from nailgun import entities
+from robottelo.containers import Container
+from robottelo.constants import (
+    RHEL_7_MAJOR_VERSION,
+    RHEL_6_MAJOR_VERSION,
+)
 
 
 class BootstrapScriptTestCase(CLITestCase):
@@ -32,19 +35,30 @@ class BootstrapScriptTestCase(CLITestCase):
         super(BootstrapScriptTestCase, cls).setUpClass()
         cls.org = entities.Organization().create()
         cls.loc = entities.Location(organization=[cls.org]).create()
-        import pdb; pdb.set_trace()
         # create an activation key
         cls.ak = entities.ActivationKey().create()
 
-        # create a host group
-        hostgroup = entities.HostGroup(
+        # Get OS ID
+        cls.os = entities.OperatingSystem().search(query={
+            u'search': u'name="RedHat" AND (major="{0}" OR major="{1}")'
+                       .format(RHEL_6_MAJOR_VERSION, RHEL_7_MAJOR_VERSION)
+                })[0].read()
+
+        cls.domain = entities.Domain(
             location=[cls.loc],
             organization=[cls.org],
+                ).create()
+
+        # create a host group
+        cls.hostgroup = entities.HostGroup(
+            location=[cls.loc],
+            organization=[cls.org],
+            operatingsystem=[cls.os.id],
         ).create()
 
     @tier4
     def test_positive_register(self):
-        """System is registered
+        """System is registered.
 
         :id: e34561fd-e0d6-4587-84eb-f86bd131aab1
 
@@ -60,19 +74,21 @@ class BootstrapScriptTestCase(CLITestCase):
 
         :CaseImportance: High
         """
-
         my_host = Container(agent=True)
+        host_name = my_host.execute("hostname")
         my_host.execute("curl -O http://{}/pub/bootstrap.py".format(settings.server.hostname))
-        my_host.execute("python bootstrap.py -l admin -s {} -o '{}' -L '{}' -g {} -a {}".format
-                (settings.server.hostname,cls.org,cls.log,hostgroup.name,cls.ak.name))
+        my_host.execute("python bootstrap.py -l admin -p changme -s {} -o '{}' \
+                         -L '{}' -g {} -a {} --fqdn {}.{}"
+                        .format(settings.server.hostname, self.org.name, self.loc.name,
+                                self.hostgroup.name, self.ak.name, host_name, self.domain))
         result = my_host.execute("subscription-manager status")
-        assert result.return_code == 0, 'Not registered'
+        assert "Registered" in result, 'Not registered'
         Container.delete(my_host)
 
     @tier1
     @upgrade
     def test_positive_reregister(self):
-        """Registered system is re-registered
+        """Registered system is re-registered.
 
         :id: d8a7aef1-7522-47a8-8478-77e81ca236be
 
@@ -87,19 +103,21 @@ class BootstrapScriptTestCase(CLITestCase):
 
         :CaseAutomation: automated
 
-        :CaseImportance: Medium
+        :CaseImportance: Mediumtests/foreman/cli/test_bootstrap_script.py
         """
-
         my_host = Container(agent=True)
-        my_host.register('{},{}'.format(settings.server.hostname,ak.name))
+        host_name = my_host.execute("hostname")
+        my_host.register('{},{}'.format(settings.server.hostname, self.ak.name))
         my_host.execute("subscription-manager attach --auto")
         # Check and assert the host is registered
-        my_host.execute("subscription-manager status")
+        result = my_host.execute("subscription-manager status")
         assert result.return_code == 0, 'Not registered'
         # register host again using bootstrap.py
         my_host.execute("curl -O http://{}/pub/bootstrap.py".format(settings.server.hostname))
-        my_host.execute("python bootstrap.py -l admin -s {} -o '{}' -L '{}' -g {} -a {}".format
-                (settings.server.hostname,cls.org,cls.log,hostgroup.name,ak.name))
+        my_host.execute("python bootstrap.py -l admin -p changme -s {} -o '{}' \
+                         -L '{}' -g {} -a {} --fqdn {}.{}".format
+                        (settings.server.hostname, self.org.name, self.loc.name,
+                         self.hostgroup.name, self.ak.name, host_name, self.domain))
         # Check and assert the host is registered
         result = my_host.execute("subscription-manager status")
         assert result.return_code == 0, 'Not registered'
@@ -108,7 +126,7 @@ class BootstrapScriptTestCase(CLITestCase):
     @tier1
     @stubbed()
     def test_positive_migrate(self):
-        """RHN registered system is migrated
+        """RHN registered system is migrated.
 
         :id: 26911dce-f2e3-4aef-a490-ad55236493bf
 
@@ -128,7 +146,7 @@ class BootstrapScriptTestCase(CLITestCase):
     @tier1
     @stubbed()
     def test_negative_register_no_subs(self):
-        """Attempt to register when no subscriptions are available
+        """Attempt to register when no subscriptions are available.
 
         :id: 26f04562-6242-4542-8852-4242156f6e45
 
@@ -147,7 +165,7 @@ class BootstrapScriptTestCase(CLITestCase):
     @tier1
     @stubbed()
     def test_negative_register_bad_hostgroup(self):
-        """Attempt to register when hostgroup doesn't meet all criteria
+        """Attempt to register when hostgroup doesn't meet all criteria.
 
         :id: 29551e22-ae63-47f2-86f3-5f1444df8493
 
@@ -167,7 +185,7 @@ class BootstrapScriptTestCase(CLITestCase):
     @tier1
     @stubbed()
     def test_positive_register_host_collision(self):
-        """Attempt to register with already created host
+        """Attempt to register with already created host.
 
         :id: ec39c981-5b8a-43a3-84f1-71871a951c53
 
@@ -187,7 +205,7 @@ class BootstrapScriptTestCase(CLITestCase):
     @tier1
     @stubbed()
     def test_negative_register_missing_sattools(self):
-        """Attempt to register when sat tools not available
+        """Attempt to register when sat tools not available.
 
         :id: 88f95080-a6f1-4a4f-bd7a-5d030c0bd2e0
 
